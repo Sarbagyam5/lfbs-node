b2e8d6415207f5c72a723f6e316859e4f627d77e
Sarbagya <sarbagyam5@gmail.com>
2025-04-11 08:35:47 +0545

    lasun

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv, { parse } from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import connectDb from "./config/database.js";
+import cors from "cors";
 
 const app = express();
 
@@ -14,6 +15,12 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("hello sathi");
 });
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/api/auth", authRoutes);
 
